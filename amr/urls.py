2326c6596a9c9b84f50034d4876520414bb3bf8f
essam-tech <essamamin756@gmail.com>
2025-09-14 19:36:21 +0300

    تعديل السعر

--- amr/urls.py
+++ amr/urls.py
@@ -1,7 +1,7 @@
 # amr/urls.py
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views   # ✅ لازم هذا السطر
+from django.contrib.auth import views as auth_views  
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -9,11 +9,11 @@ urlpatterns = [
     path("product/<slug:product_slug>/", views.product_detail, name="product_detail"),
     path("cart/add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    path("cart/", views.cart_view, name="cart_view"),  # ✅ هذا هو المطلوب
+    path("cart/", views.cart_view, name="cart_view"), 
     path("cart/add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
-    path("cart/update/<int:item_id>/", views.update_cart_item, name="update_cart_item"),  # ✅
-    path("checkout/", views.checkout, name="checkout"),  # ✅
-    path('cart/add/<int:product_id>/', views.add_to_cart, name='cart_add'),  # <--- هذا
+    path("cart/update/<int:item_id>/", views.update_cart_item, name="update_cart_item"),  
+    path("checkout/", views.checkout, name="checkout"),  
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='cart_add'),
 
 
 ]
